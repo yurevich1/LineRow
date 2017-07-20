28cb805b26e7271e1d5381f063bd799388d08931
Admin <admin@Admins-Mac.Dlink>
2017-07-20 07:45:00 +0300

    carthage

---
+++ Example/LineRowCarthage/LineRowCarthage.h
@@ -0,0 +1,19 @@
+//
+//  LineRowCarthage.h
+//  LineRowCarthage
+//
+//  Created by Admin on 19.07.17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LineRowCarthage.
+FOUNDATION_EXPORT double LineRowCarthageVersionNumber;
+
+//! Project version string for LineRowCarthage.
+FOUNDATION_EXPORT const unsigned char LineRowCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LineRowCarthage/PublicHeader.h>
+
+
