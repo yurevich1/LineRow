5a0174e9d23bbfe507abe62f57e0c9d2e3ad333b
Admin <admin@Admins-Mac.Dlink>
2017-07-20 07:49:55 +0300

    Release 1.0.0

---
+++ Example/Pods/Target Support Files/LineRow/LineRow-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LineRow : NSObject
+@end
+@implementation PodsDummy_LineRow
+@end
---
+++ Example/Pods/Target Support Files/Pods-LineRow_Tests/Pods-LineRow_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LineRow_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LineRow_Tests
+@end
